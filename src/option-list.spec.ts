96978c6c8db5c31fd9076fbf6143d5020b784aeb
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2017-03-11 21:57:05 +0100

    Unit test.

--- src/option-list.spec.ts
+++ src/option-list.spec.ts
@@ -1,5 +1,6 @@
 import {OptionList} from './option-list';
 import {Option} from './option';
+import {IOption} from './option.interface';
 
 let numbers = [0, 1, 2, 3, 4];
 
@@ -14,7 +15,7 @@ describe('An OptionList\'s getOptionsByValue function', () => {
     let optionList: OptionList;
 
     beforeEach(() => {
-        let options: Array<any> = numbers.map((i) => {
+        let options: Array<IOption> = numbers.map((i) => {
             return {
                 label: `Option ${i}`,
                 value: `${i}`
@@ -53,5 +54,53 @@ describe('An OptionList\'s getOptionsByValue function', () => {
         expect(result.length).toBe(2);
         expect(result[0].value).toBe('3');
         expect(result[1].value).toBe('3');
+    });
+});
+
+describe('An OptionList\'s get value function', () => {
+
+    let optionList: OptionList;
+
+    beforeEach(() => {
+        let options: Array<IOption> = numbers.map((i) => {
+            return {
+                label: `Option ${i}`,
+                value: `${i}`
+            };
+        });
+        optionList = new OptionList(options);
+    });
+
+    it('returns empty list if list of options is empty', () => {
+        optionList = new OptionList([]);
+        let value: Array<string> = optionList.value;
+
+        expect(value.constructor).toBe(Array);
+        expect(value.length).toBe(0);
+    });
+
+    it('returns empty list if no options are selected', () => {
+        let value: Array<string> = optionList.value;
+
+        expect(value.constructor).toBe(Array);
+        expect(value.length).toBe(0);
+    });
+
+    it('returns value of selected option', () => {
+        optionList.options[1].selected = true;
+        let value: Array<string> = optionList.value;
+
+        expect(value.length).toBe(1);
+        expect(value[0]).toBe('1');
+    });
+
+    it('returns value of selected options', () => {
+        optionList.options[2].selected = true;
+        optionList.options[4].selected = true;
+        let value: Array<string> = optionList.value;
+
+        expect(value.length).toBe(2);
+        expect(value[0]).toBe('2');
+        expect(value[1]).toBe('4');
     });
 });
