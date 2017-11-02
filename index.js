55aaa38ae98f354c8deaad42f3cae63a32358f16
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2017-11-02 12:48:07 +0100

    Update metadata version to 4 in index.metadata.json.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require('@angular/core');
 var common_1 = require('@angular/common');
 var forms_1 = require('@angular/forms');
