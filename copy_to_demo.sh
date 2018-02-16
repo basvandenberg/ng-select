b0a7246e7d2a201c0fcbce10dc00250305f755f0
Dan Clegg <dgclegg@gmail.com>
2018-02-16 16:14:01 -0700

    Changed rmdir to rm -fR. Removed incorrect deep package path

--- copy_to_demo.sh
+++ copy_to_demo.sh
@@ -2,9 +2,9 @@
 rm -v src/app/ng-select/dist/ng-select-*.tgz
 rm -rv demo/node_modules/ng-select/*
 
-npm pack src/app/ng-select/dist
+npm pack dist
 tar -xzvf ng-select-*.tgz
 mv -v package/* demo/node_modules/ng-select/
 
 rm ng-select-*.tgz
-rmdir -v package
+rm -fRv package
