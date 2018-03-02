59b9076885aafbd7f41cec8bc1f15ba679047c9c
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2018-03-02 21:57:01 +0100

    Fix copy current version to demo for testing.

--- copy_to_demo.sh
+++ copy_to_demo.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-rm -v src/app/ng-select/dist/ng-select-*.tgz
 rm -rv demo/node_modules/ng-select/*
 
-npm pack dist
+npm pack ./dist
 tar -xzvf ng-select-*.tgz
 mv -v package/* demo/node_modules/ng-select/
 
 rm ng-select-*.tgz
-rm -fRv package
+rmdir package
